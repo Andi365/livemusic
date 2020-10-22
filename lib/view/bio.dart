7659858b4d065e4b8486f9e98ea01a5f5a1073f4
Andi365 <anders1998@live.dk>
2020-10-22 18:01:19 +0200

    Added expandable bio

--- lib/view/bio.dart
+++ lib/view/bio.dart
@@ -1,3 +1,4 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 
 class Bio extends StatelessWidget {
@@ -9,34 +10,40 @@ class Bio extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(20),
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-            /*decoration: BoxDecoration(
-              border: Border.all(10)
-            ),*/
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Biography',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Color.fromRGBO(193, 160, 80, 1),
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ),
-          Text(
-            descText,
+      child: ExpandablePanel(
+        // ignore: deprecated_member_use
+        iconColor: Color.fromRGBO(242, 153, 74, 1),
+        header: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Biography',
             style: TextStyle(
-              fontSize: 12,
-              color: Color.fromRGBO(255, 255, 255, 1),
-              backgroundColor: Color.fromRGBO(20, 20, 20, 1),
+              fontSize: 18,
+              color: Color.fromRGBO(193, 160, 80, 1),
             ),
+            textAlign: TextAlign.left,
           ),
-        ],
+        ),
+        collapsed: Text(
+          descText,
+          softWrap: true,
+          maxLines: 3,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            fontSize: 12,
+            color: Color.fromRGBO(255, 255, 255, 1),
+            backgroundColor: Color.fromRGBO(20, 20, 20, 1),
+          ),
+        ),
+        expanded: Text(
+          descText,
+          softWrap: true,
+          style: TextStyle(
+            fontSize: 12,
+            color: Color.fromRGBO(255, 255, 255, 1),
+            backgroundColor: Color.fromRGBO(20, 20, 20, 1),
+          ),
+        ),
       ),
     );
   }
