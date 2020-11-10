916cf77eb3d8172c7b8fc5b198503075e3b65ad6
PomFritzen <nicklas.fritzen@hotmail.com>
2020-11-10 15:05:25 +0100

    saving changes

--- lib/view/profilePage.dart
+++ lib/view/profilePage.dart
@@ -44,7 +44,7 @@ class _Profilepage extends State<ProfilePage> {
       for (int i = 0; i < ratingNotifier.ratingList.length; i++) {
         ratings.add(ratingNotifier.ratingList[i].rating);
         bands.add(ratingNotifier.ratingList[i].artistName.toString());
-        dates.add(ratingNotifier.ratingList[i].date.toDate().toString());
+        dates.add(ratingNotifier.ratingList[i].wasCreated.toDate().toString());
       }
 
       for (int i = 0; i < ratings.length; i++) {
