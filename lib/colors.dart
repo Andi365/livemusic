8588c0e07b910f716576dfadb09c055de73bd0e4
Andi365 <anders1998@live.dk>
2020-11-21 21:39:23 +0100

    Changed some colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 const backgroundColor = Color.fromRGBO(20, 20, 20, 1);
+const secondaryBackgroundColor = Color.fromRGBO(48, 44, 45, 1);
 const primaryColor = Color.fromRGBO(193, 160, 80, 1);
+const secondaryColor = Color.fromRGBO(242, 153, 74, 1);
 const primaryWhiteColor = Color.fromRGBO(255, 255, 255, 1);
